86b18dd8239d04b9193d2a51587943189fd4ddc7
Cemil Kerimoglu <cemil.a.kerimoglu@gmail.com>
2023-02-03 23:44:56 +0100

    establish the basic page layout

---
+++ personal_blog/pages/index.js
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import Categories from "../components/Categories";
+import PostCard from "../components/PostCard";
+import PostWidget from "../components/PostWidget";
+
+const posts = [
+  { title: "React Testing", excerpt: "Learn React Testing" },
+  { title: "React with Tailwind", excerpt: "Learn React with Tailwind" },
+];
+
+const Home = () => {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <Head>
+        <title>Personal Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post) => (
+            <PostCard post={post} key={post.title} />
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
