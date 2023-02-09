de52f3e54963783d9f577d0a325faa0781bd1a3e
Cemil Kerimoglu <cemil.a.kerimoglu@gmail.com>
2023-02-09 22:59:45 +0100

    fetch posts from backend

--- personal_blog/pages/index.js
+++ personal_blog/pages/index.js
@@ -2,13 +2,9 @@ import Head from "next/head";
 import Categories from "../components/Categories";
 import PostCard from "../components/PostCard";
 import PostWidget from "../components/PostWidget";
+import { getPosts } from "../services";
 
-const posts = [
-  { title: "React Testing", excerpt: "Learn React Testing" },
-  { title: "React with Tailwind", excerpt: "Learn React with Tailwind" },
-];
-
-const Home = () => {
+const Home = ({ posts }) => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -17,7 +13,7 @@ const Home = () => {
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post) => (
+          {posts?.map((post) => (
             <PostCard post={post} key={post.title} />
           ))}
         </div>
@@ -33,3 +29,11 @@ const Home = () => {
 };
 
 export default Home;
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts },
+  };
+}
