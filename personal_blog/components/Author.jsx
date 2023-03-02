81fe45472802fa76d006f637a8fe745eef6ba2d9
Cemil Kerimoglu <cemil.a.kerimoglu@gmail.com>
2023-03-02 01:27:11 +0100

    start working on comments

--- personal_blog/components/Author.jsx
+++ personal_blog/components/Author.jsx
@@ -1,15 +1,19 @@
 import React from "react";
+import Image from "next/image";
 
 const Author = ({ author }) => {
   return (
-    <div className="text-center mt-20 mb-8 p-12 rounded-lg bg-black bg-opacity-20">
-      <div className="left-0 right-2 top-2">
-        <img
-          alt={author.name}
-          className="align-middle rounded-full h-28 "
-          src={author.photo.url}
-        />
-      </div>
+    <div className="flex flex-col items-center text-center mt-20 mb-8 p-12 rounded-lg bg-black bg-opacity-20">
+      <Image
+        alt={author.name}
+        unoptimized
+        width={110}
+        height={32}
+        className="align-middle rounded-full h-28"
+        src={author.photo.url}
+      />
+      <h3 className="text-white my-4 text-xl font-bold">{author.name}</h3>
+      <p className="text-white text-lg">{author.bio}</p>
     </div>
   );
 };
