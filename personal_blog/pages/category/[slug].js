4de6636a02876cd17cc1062209b3e046798cbee9
Cemil Kerimoglu <cemil.a.kerimoglu@gmail.com>
2023-05-02 21:46:40 +0200

    reorder from latest

--- personal_blog/pages/category/[slug].js
+++ personal_blog/pages/category/[slug].js
@@ -13,13 +13,19 @@ const CategoryPost = ({ posts }) => {
     return <Loader />;
   }
 
+  console.log("posts original", posts);
+  console.log("posts reversed", posts.slice(0).reverse());
+
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8">
-          {posts.map((post, index) => (
-            <PostCard key={index} post={post.node} />
-          ))}
+          {posts
+            .slice(0)
+            .reverse()
+            .map((post, index) => (
+              <PostCard key={index} post={post.node} />
+            ))}
         </div>
         <div className="col-span-1 lg:col-span-4">
           <div className="relative lg:sticky top-8">
--- personal_blog/pages/index.js
+++ personal_blog/pages/index.js
@@ -9,15 +9,18 @@ const Home = ({ posts }) => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
-        <title>Personal Blog</title>
+        <title>Insightful History</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts?.map((post) => (
-            <PostCard post={post} key={post.title} />
-          ))}
+          {posts
+            ?.slice(0)
+            .reverse()
+            .map((post) => (
+              <PostCard post={post} key={post.title} />
+            ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
